a16e836af82c8617b61e33606766a0a7ed6f361d
James Anderson <james.p.anderson16@gmail.com>
2017-02-04 17:15:07 -0700

    finish blog comments feature

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-exit# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,13 +10,22 @@ exit# This file is auto-generated from the current state of the database. Instea
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204205730) do
+ActiveRecord::Schema.define(version: 20170204233215) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
